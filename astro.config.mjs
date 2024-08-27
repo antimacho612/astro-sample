d5b8fcda843dd56851868e957ed6b4a090533007
antimacho <58581449+antimacho612@users.noreply.github.com>
2024-08-27 20:50:46 +0900

    add mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,27 +1,32 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import mdx from '@astrojs/mdx';
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		starlight({
-			title: 'My Docs',
-			social: {
-				github: 'https://github.com/withastro/starlight',
-			},
-			sidebar: [
-				{
-					label: 'Guides',
-					items: [
-						// Each item here is one entry in the navigation menu.
-						{ label: 'Example Guide', slug: 'guides/example' },
-					],
-				},
-				{
-					label: 'Reference',
-					autogenerate: { directory: 'reference' },
-				},
-			],
-		}),
-	],
+  integrations: [
+    starlight({
+      title: "Astroサンプル",
+
+      tableOfContents: {
+        maxHeadingLevel: 6,
+      },
+      sidebar: [
+        {
+          label: "はじめに",
+          link: "/",
+        },
+        {
+          label: "コンポーネント",
+          autogenerate: { directory: "components" },
+        },
+        {
+          label: "メモ",
+          autogenerate: { directory: "memos" },
+        },
+      ],
+      customCss: ["./src/styles/custom.css"],
+    }),
+    mdx(),
+  ],
 });
---
+++ src/content/docs/components/example2.md
@@ -0,0 +1,20 @@
+---
+title: "サンプルページ②"
+---
+
+```json
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  // Enable Custom Markdown options, plugins, etc.
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula',
+      // Learn more about this configuration here:
+      // https://docs.astro.build/en/guides/markdown-content/#syntax-highlighting
+    },
+  },
+});
+```
